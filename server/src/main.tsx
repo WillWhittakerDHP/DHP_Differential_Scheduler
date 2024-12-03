ba1fc7ab93000fad7fe7e2009bd49d47233fce10
WillWhittakerDHP <will@districthomepro.com>
2024-12-03 12:56:20 -0500

    why do i need to commit again again?

--- server/src/main.tsx
+++ server/src/main.tsx
@@ -1,47 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-import App from './App.tsx'
-import AvailavilityPage from './pages/AvailavilityPage.tsx';
-import ConfirmationPage from './pages/ConfirmationPage.tsx';
-import ParticipantInformationPage from './pages/ParticipantInformationPage.tsx';
-import PropertyInformationPage from './pages/PropertyInformationPage.tsx';
-import ServiceSelectionPage from './pages/ServiceSelectionPage.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        //index: true,
-        path: '/AvailavilityPage',
-        element: <AvailavilityPage />,
-      },
-      {
-        path: '/ConfirmationPage',
-        element: <ConfirmationPage />,
-      },
-      {
-        path: '/ParticipantInformationPage',
-        element: <ParticipantInformationPage />,
-      },
-      {
-        path: '/PropertyInformationPage',
-        element: <PropertyInformationPage />,
-      },
-      {
-        path: '/ServiceSelectionPage',
-        element: <ServiceSelectionPage />,
-      },
-    ],
-  },
-]);
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
-};
-
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
