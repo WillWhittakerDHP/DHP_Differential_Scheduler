dbeca066bb508c340e22a71bd593f944e6d2cdc5
WillWhittakerDHP <will@districthomepro.com>
2026-03-21 10:52:52 -0400

    [8.1.1.2] tier-end: commit remaining work

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import helmet from 'helmet'
 import cors from 'cors'
 import morgan from 'morgan'
 import routes from './routes/index.js'
+import { getCorsOrigin } from './config/envConfig.js'
 import { notFound, errorHandler } from './middlewares/index.js'
 import { initializeDatabase } from './config/app.js'
 import { loadTokensFromFile } from './config/googleOAuth.js'
@@ -35,7 +36,7 @@ startServer()
 
 app.use(morgan('dev'))
 app.use(helmet())
-app.use(cors())
+app.use(cors({ origin: getCorsOrigin() }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use((req, _res, next) => {
